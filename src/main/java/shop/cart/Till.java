e7642f87f2ea93251ec6695cb5d80d4df4a06d02
Ryszard Madejski <r.madejski@kainos.com>
2015-05-07 14:13:06 +0200

    Add Till implementation

--- src/main/java/shop/cart/Till.java
+++ src/main/java/shop/cart/Till.java
@@ -1,21 +1,32 @@
 package shop.cart;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
+import java.util.ArrayList;
 import java.util.List;
 
 public class Till {
 
+    private List<Product> products;
+    private int receiptValue;
+
+    public Till() {
+        products = new ArrayList<Product>();
+        receiptValue = 0;
+    }
+
     /**
      * Adds a list of shop.cart.products to the Till
      *
-     * @param products list of shop.cart.products to be paid for
+     * @param newProducts list of shop.cart.products to be paid for
      */
-    public void addProducts(List<Product> products) {
-        // TODO: add method implementation
-        throw new NotImplementedException();
-    }
+    public void addProducts(List<Product> newProducts) {
+        if(newProducts != null) {
+            products.addAll(newProducts);
 
+            for(Product p: newProducts) {
+                receiptValue += p.getPrice();
+            }
+        }
+    }
 
     /**
      * Gets the list of shop.cart.products registered in the Till
@@ -23,8 +34,7 @@ public class Till {
      * @return current product list
      */
     public List<Product> getProducts() {
-        // TODO: add method implementation
-        throw new NotImplementedException();
+        return products;
     }
 
     /**
@@ -33,8 +43,7 @@ public class Till {
      * @return current value of shop.cart.products in pence
      */
     public int getReceiptValue() {
-        // TODO: add method implementation
-        throw new NotImplementedException();
+        return receiptValue;
     }
 
 }
